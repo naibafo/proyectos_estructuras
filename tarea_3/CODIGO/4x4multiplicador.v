96c44223fd8395ba8e5df2cb15b55fabfae4fe31
frinconr <frinconr95@gmail.com>
2015-05-14 21:54:28 -0600

    Multiplicador de 4 entradas

--- tarea_3/CODIGO/4x4multiplicador.v
+++ tarea_3/CODIGO/4x4multiplicador.v
@@ -1,24 +1,30 @@
-module 4x4Multiplicador (
+`include "Mult_Controller.v"
 
 
-	input	wire	[31:0]	iData_A, 	// 	Input data A  
-	input	wire	[31:0]	iData_B,	// 	Input data B
-	input	wire	[31:0]	iData_C, 	// 	Input data C  
-	input	wire	[31:0]	iData_D, 	// 	Input data D  
+module Four_Multiplicador # (parameter SIZE=32, parameter COUNTER_SIZE=5)
+(
+
+
+	input	wire	[SIZE-1:0]	iData_A, 	// 	Input data A  
+	input	wire	[SIZE-1:0]	iData_B,	// 	Input data B
+	input	wire	[SIZE-1:0]	iData_C, 	// 	Input data C  
+	input	wire	[SIZE-1:0]	iData_D, 	// 	Input data D  
 	input 	wire	Clock,
 	input 	wire	Reset,
 	input 	wire 	iValid_Data,		// 	Input flag that indicates when the inputs of data are valid
 	input	wire	iAcknoledged,		//	Input flag that sends the state machine to Idle State
-	output	reg		oDone,				//	Output flag that indicates when the data is ready 
-	output	reg		oIdle,				//	Output flag that indicates when the data is ready
-	output	wire 	[31:0]	oResult
+	output	wire		oDone,				//	Output flag that indicates when the data is ready 
+	output	wire		oIdle,				//	Output flag that indicates when the data is ready
+	output	wire 	[4*SIZE-1:0]	oResult
 );
 
-	reg Enable;
+	wire Enable;
+	wire [2*SIZE-1:0] Result1 ;
+	wire [2*SIZE-1:0] Result2;
 	
 	assign Enable = Done1 & Done2;
 
-	Multiplicator mult1
+	Multiplicator  # (SIZE, COUNTER_SIZE) mult1 
 	(
 		.iData_A(iData_A),
 		.iData_B(iData_B),	
@@ -32,7 +38,7 @@ module 4x4Multiplicador (
 	);
 	
 	
-		Multiplicator mult2
+		Multiplicator # (SIZE, COUNTER_SIZE) mult2 
 	(
 		.iData_A(iData_C),
 		.iData_B(iData_D),	
@@ -45,7 +51,7 @@ module 4x4Multiplicador (
 		.oResult(Result2)
 	);
 	
-		Multiplicator mult3
+		Multiplicator # (2*SIZE, COUNTER_SIZE+1) mult3 
 	(
 		.iData_A(Result1),
 		.iData_B(Result2),	
