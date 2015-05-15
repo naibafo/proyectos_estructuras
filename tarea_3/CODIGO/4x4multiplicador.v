b7ea8dbfcca8fd18f2e15a212fba81fd6d0ab4ec
frinconr <frinconr95@gmail.com>
2015-05-14 23:15:59 -0600

    oIdle 4x4

--- tarea_3/CODIGO/4x4multiplicador.v
+++ tarea_3/CODIGO/4x4multiplicador.v
@@ -22,6 +22,7 @@ module Four_Multiplicador # (parameter SIZE=32, parameter COUNTER_SIZE=5)
 	wire [2*SIZE-1:0] Result1 ;
 	wire [2*SIZE-1:0] Result2;
 	
+	assign oIdle = Idle1 & Idle2 & Idle3; 
 	assign Enable = Done1 & Done2;
 
 	Multiplicator  # (SIZE, COUNTER_SIZE) mult1 
@@ -60,7 +61,7 @@ module Four_Multiplicador # (parameter SIZE=32, parameter COUNTER_SIZE=5)
 		.iValid_Data(Enable),	
 		.iAcknoledged(iAcknoledged),		 
 		.oDone(oDone),				
-		.oIdle(oIdle),				
+		.oIdle(Idle3),				
 		.oResult(oResult)
 	);	
 
