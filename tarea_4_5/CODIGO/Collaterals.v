c082ee4285a0c4b6576a9d433831584a32978b15
Fabian Melendez <fabmelb@gmail.com>
2015-06-15 16:57:41 -0600

    Start with Instruction Decode

---
+++ tarea_4_5/CODIGO/Collaterals.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+////////////////////////////////////////////////////////////////////////
+//// MODULE: UPCOUNTER                                                //
+////////////////////////////////////////////////////////////////////////
+
+module UPCOUNTER_POSEDGE # (parameter SIZE=16)
+(
+input wire Clock, Reset,
+input wire [SIZE-1:0] Initial,
+input wire Enable,
+output reg [SIZE-1:0] Q
+);
+
+
+  always @(posedge Clock )
+  begin
+      if (Reset)
+        Q = Initial;
+      else
+		begin
+		if (Enable)
+			Q = Q + 1;
+			
+		end			
+  end
+
+endmodule
+
+////////////////////////////////////////////////////////////////////////
+//// MODULE: FFD                                                      //
+////////////////////////////////////////////////////////////////////////
+/*
+	Bank of FFD, posedge driven and with sinchronous reset
+*/
+module FFD # ( parameter SIZE=8 )
+(
+	input wire				Clock,
+	input wire				Reset,
+	input wire				Enable,
+	input wire [SIZE-1:0]	D,
+	output reg [SIZE-1:0]	Q
+);
+	
+
+always @ (posedge Clock) 
+begin
+	if ( Reset )
+		Q <= 0;
+	else
+	begin	
+		if (Enable) 
+			Q <= D; 
+	end	
+ 
+end
+
+endmodule
+
+
---
+++ tarea_4_5/CODIGO/ID/InstructionDecode.v
@@ -0,0 +1,13 @@
+/*
+	MODULE:
+		Instruction Decode
+*/
+
+module InstructionDecoder
+(
+	input wire	 		Clock,							// 	Input Clock
+	input wire 		Reset,							// 	Reset signal
+	
+);
+
+endmodule
