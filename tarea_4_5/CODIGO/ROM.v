60d0aa9777401418a03bfa2221b17b2e83843a0d
Fabian Melendez <fabmelb@gmail.com>
2015-06-30 12:55:37 -0600

    DEF

--- tarea_4_5/CODIGO/ROM.v
+++ tarea_4_5/CODIGO/ROM.v
@@ -9,15 +9,13 @@ always @ ( iAddress )
 begin
 	case (iAddress)
 	
-	0: oInstruction = { `NOP  , 10'b0};
-	1: oInstruction = { `LDCA , 10'b0};
-	2: oInstruction = { `LDCB , 10'b1};
-	3: oInstruction = { `NOP  , 10'b0};
-	4: oInstruction = { `ADDA , 10'b0};
-	5: oInstruction = { `NOP  , 10'b0};
-	6: oInstruction = { `ADDA , 10'b0};
-	7: oInstruction = { `NOP  , 10'b0};
-	8: oInstruction = { `BAMI , 10'b1111111011};
+	0: oInstruction = { `LDA , 10'b1111100000};
+	1: oInstruction = { `LDB , 10'b0000011111};
+	2: oInstruction = { `ADDCA , 10'b1111111111};
+	3: oInstruction = { `ADDCB , 10'b1010101010};
+	4: oInstruction = { `STB , 10'b0};
+	5: oInstruction = { `STA , 10'b1};
+	6: oInstruction = { `BAEQ , 10'b0};
 
 
 	default:
